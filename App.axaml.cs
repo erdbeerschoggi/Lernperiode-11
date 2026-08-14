259b1b40fb0cbc2846ad05026f5958159a13deed
Maja Halszka Piotrowska <M.Piotrowska.inf24@stud.bbbaden.ch>
2026-08-14 11:27:07 +0200

    Add files via upload

---
+++ App.axaml.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using Lernperiode11.ViewModels;
+using Lernperiode11.Views;
+
+namespace Lernperiode11
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
