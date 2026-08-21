400a9d51d95a0667e255411a93a8e708ae29c1c1
Maja Halszka Piotrowska <M.Piotrowska.inf24@stud.bbbaden.ch>
2026-08-21 11:25:38 +0200

    Add files via upload

---
+++ App/KletterRouten.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KletterRoutenBasis.Models
+{
+    public class KletterRouten
+    {
+
+        public bool IsChecked { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
---
+++ App/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace KletterRoutenBasis.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
